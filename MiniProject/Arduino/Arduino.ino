bd9680ec2ae44900532a1f1fd093e30a8d7a9986
mluyten <matt.luyten@gmail.com>
2021-02-24 14:42:55 -0700

    Updated Arduino.ino with PI controller

--- MiniProject/Arduino/Arduino.ino
+++ MiniProject/Arduino/Arduino.ino
@@ -3,17 +3,17 @@
 #define SLAVE_ADDRESS 0x04
 
 /* Name: Group 6 | EENG350 | Mini Project: 4.6
+ 
+ Purpose: Step response experiment
+ 
+ How to Use:
+ 
+ NOTE: This hasn't been tested at all and probably doesn't even work right now. 
+ I just ran out of time and can't work on it anymore tonight.
+ 
+ */
 
-   Purpose: Step response experiment
-
-   How to Use:
-
-   NOTE: This hasn't been tested at all and probably doesn't even work right now. 
-   I just ran out of time and can't work on it anymore tonight.
-
-*/
-
-int sampleRate = 1; // 5 milliseconds
+int sampleRate = 5; // milliseconds
 int desiredPWM = 255;
 
 int driveModePin = 4;    // drive/break mode when HIGH, drive/coast mode when LOW
@@ -22,13 +22,25 @@ int m1DirPin = 7;       // sign of voltage
 int m1SpeedPin = 9;     // voltage
 
 byte data[32];
-byte posArray[3] = {2, 0, 0};
+byte posArray[3] = {
+  2, 0, 0};
 
-int pos = 0;
-int lastPos = 0;
+int thetaCurrent = 0;
+int thetaSet = 800;
 double angularVelocity = 0.0;
+int cmd = 0;
+boolean cmdReceived = false;
 int i = 0;
 int j = 0;
+
+
+float u = 0.0;  //PI Output
+//float Kp = 0.0871, Kd = 0, Ki = 0.00291;                    //Controller parameters
+//float Kp = 0.25701, Kd = 0, Ki = 0.00291;
+float Kp = 0.3, Kd = 0, Ki = 0.00291;
+float I = 0.0, thetaDelta = 0.0;    //Variables that the program calculated as coefficients for the controller parameters
+float voltageToDuty = 255 / 7.4;
+unsigned long Ts = 0, Tc = millis();
 
 Encoder myEnc(2,3);
 
@@ -36,11 +48,11 @@ void setup() {
   Serial.begin(115200); // start serial for output
   // initialize i2c as slave
   Wire.begin(SLAVE_ADDRESS);
-  
+
   // define callbacks for i2c communication
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
-  
+
   pinMode(driveModePin, OUTPUT);
   digitalWrite(driveModePin, HIGH);
 
@@ -53,40 +65,53 @@ void setup() {
 void loop() {
   unsigned long timeBefore = micros();
 
-  make_step();
   // Read encoder and stuff
-  lastPos = pos;
-  pos = abs(myEnc.read() % 3200);
-  posArray[1] = pos & 255;
-  posArray[2] = pos / 256;
-  angularVelocity = ((double) (pos - lastPos)) * 2.0 * 3.14159 / 3200.0 / (0.001 * sampleRate);
+  thetaCurrent = abs(myEnc.read() % 3200);
+  posArray[1] = thetaCurrent & 255;
+  posArray[2] = thetaCurrent / 256;
+  //angularVelocity = ((double) (thetaCurrent - lastThetaCurremt)) * 2.0 * 3.14159 / 3200.0 / (0.001 * sampleRate);
+
+  thetaDelta = thetaSet - thetaCurrent;
   
+  if (thetaDelta < 0) {
+    thetaDelta = 3200 + thetaDelta;
+  }
+  
+  I = I + sampleRate * thetaDelta;                          // Define the integral term
+
+  u = Kp * thetaDelta + Ki * 2;            // Put it all together to get the resulting position change
+  analogWrite(m1SpeedPin, u);   // Output this result to the motor
+  if (u < 0) {
+    digitalWrite(m1DirPin, LOW);
+  } else {
+    digitalWrite(m1DirPin, HIGH);
+  }
+
+  Serial.print(u);
+  Serial.print("\t");
+  Serial.println(thetaDelta);
+
   unsigned long timeNow = micros();
   unsigned long timeMain = timeNow - timeBefore; // total time spent doing things
 
   if ( timeMain > (sampleRate * pow(10, 3)) ) {
     Serial.println("Main takes too long.");
-  } else {
+  } 
+  else {
     while ( micros() < timeNow + (sampleRate * pow(10, 3) - timeMain) ); // take a sample every 5ms = 5000us
-    
-    // Insert serial data here (time, motor voltage, angular velocity)
-    //Serial.print( millis() );
-    //Serial.print(" ");
-    //Serial.print(analogRead(m1SpeedPin));
-    Serial.print(" ");
-    Serial.println(angularVelocity);
-    
+
   }
 }
 
 void make_step() { // Sets motor speed to 0 before 1 sec, then a positive speed after 1 sec
   if (millis() < 1000) {
     analogWrite(m1SpeedPin, 0);
-  } else {
+  } 
+  else {
     analogWrite(m1SpeedPin, desiredPWM);
   }
 }
- 
+
 // callback for received data
 void receiveData(int byteCount){
   i = 0;
@@ -95,12 +120,17 @@ void receiveData(int byteCount){
     i++;
   }
   i--;
-  
+
+  if ((data[1] != cmd) && (data[1] != 0)) {
+    cmdReceived = true;
+    cmd = data[1];
+  }
+
 }
 
 void sendData() {
   if (i == 0) {
-    if (data[0] == 1) {
+    if (data[0] == 255) {
       j = 0;
       data[0] = 0;
     }
@@ -110,3 +140,5 @@ void sendData() {
     Wire.write(posArray[j]);
   }
 }
+
+
