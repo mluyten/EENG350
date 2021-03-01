197affbc3bd4cc1c5099305265dbcf05e663dfad
linleem <79241543+linleem@users.noreply.github.com>
2021-02-28 19:35:47 -0700

    Update Step_Response.ino

--- MiniProject/Reference Code/Step_Response.ino
+++ MiniProject/Reference Code/Step_Response.ino
@@ -1,7 +1,6 @@
 /* Name: Group 6 | EENG350 | Mini Project: 4.6
 
-   Purpose: Step response experiment (old).
-   Working step response code can be found in MP_Final_Arduino.ino
+   Purpose: Step response experiment
 
 */
 
@@ -21,18 +20,18 @@ void setup() {
 void loop() {
   unsigned long timeBefore = micros();
 
-  make_step();
-  // Read encoder and stuff
+  make_step(); // Step response
 
   unsigned long timeNow = micros();
-  unsigned long timeMain = timeNow - timeBefore; // total time spent doing things
+  unsigned long timeMain = timeNow - timeBefore; // total time spent doing things in main
 
-  if ( timeMain > (sampleRate * pow(10, 3)) ) {
+  if ( timeMain > (sampleRate * pow(10, 3)) ) { // Error checking to make sure main doesn't take longer than the sampling rate
     Serial.println("Main takes too long.");
-  } else if ( micros() >= timeNow + (sampleRate * pow(10, 3) - timeMain) ) {
-    // take a sample every 5ms = 5000us
   }
-}
+  else {
+    while ( micros() < timeNow + (sampleRate * pow(10, 3) - timeMain) ); // wait to take a sample exactly every 5ms = 5000us
+         //Print samples here
+  }
 
 
 void make_step() { // Sets motor speed to 0 before 1 sec, then a positive speed after 1 sec
