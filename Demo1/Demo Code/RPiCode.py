908e57cda78de300abd2a3eeee6b060115bbead0
mluyten <74384991+mluyten@users.noreply.github.com>
2021-03-22 13:36:01 -0600

    Update RPiCode.py

--- Demo1/Demo Code/RPiCode.py
+++ Demo1/Demo Code/RPiCode.py
@@ -1,4 +1,4 @@
-# Group 6 | EENG350 | Mini Project RasPi Integration Code
+# Group 6 | EENG350 | Demo 1 RasPi Integration Code
 # Purpose: Runs CV program and tells the Arduino the desired angle. 
 #          Updates the desired and current position of the wheel on the LCD display.
 
@@ -24,14 +24,18 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Initialise the LCD class
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 
+# Clears the LCD
 lcd.color = [0, 0, 0]
 lcd.clear()
 
+# This is a angle-reading thread. It runs until runThread is set to false.
+# This function reads the angle of the center of an aruco marker to the center plane of the robot and displays it on the LCD. 
+# If no aruco is detected, "No Waypoint Detected"
 def readPosition():
     angle = -1;
     while(runThread):
-        lcd.color = [0,0,100]
-        angle = -1 * cam.getArucoAngle()
+        lcd.color = [0,0,100] # Turn LCD on
+        angle = -1 * cam.getArucoAngle() # Get angle from CV code
         if angle == -1:
             lcd.message = "No Waypoint     \nDetected        "
         else:
@@ -41,12 +45,13 @@ def readPosition():
         
 runThread = False
 
-print("Searching for Waypoint...")
+# Initializes and starts the thread.
+print("Searching for Waypoint...") 
 lcd.clear()
 runThread = True
 thread = threading.Thread(target=readPosition)
 thread.start()
-x = input("Press Any Key to Stop")
+x = input("Press Any Key to Stop") # Waits for keystoke to stop thread.
 print("Done!")
 runThread = False
 
